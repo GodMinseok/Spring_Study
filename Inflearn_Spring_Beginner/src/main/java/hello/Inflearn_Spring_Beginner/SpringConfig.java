238a8fbe47fdd4b2d9af3c61d02bc47f3eb9246a
GodMinseok <mseok830@naver.com>
2024-07-16 20:34:34 +0900

    Section 6. 스프링 DB 접근 기술 (순수 JDBC)

--- Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/SpringConfig.java
+++ Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/SpringConfig.java
@@ -1,18 +1,27 @@
 package hello.Inflearn_Spring_Beginner;
 
+import hello.Inflearn_Spring_Beginner.repository.JdbcMemberRepository;
 import hello.Inflearn_Spring_Beginner.repository.MemberRepository;
 import hello.Inflearn_Spring_Beginner.repository.MemoryMemberRepository;
 import hello.Inflearn_Spring_Beginner.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
