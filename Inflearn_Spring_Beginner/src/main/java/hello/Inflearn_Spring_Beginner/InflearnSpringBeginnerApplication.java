93ae8546487b56303f34d74c2e03a37f30dfc1a5
GodMinseok <mseok830@naver.com>
2024-07-16 16:21:12 +0900

    인프런 입문편 시작

---
+++ Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/InflearnSpringBeginnerApplication.java
@@ -0,0 +1,13 @@
+package hello.Inflearn_Spring_Beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringBeginnerApplication.class, args);
+	}
+
+}
---
+++ Inflearn_Spring_Beginner/src/test/java/hello/Inflearn_Spring_Beginner/InflearnSpringBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.Inflearn_Spring_Beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
