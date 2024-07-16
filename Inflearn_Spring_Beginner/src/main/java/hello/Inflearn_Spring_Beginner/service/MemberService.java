c972a693928d1fe4e40d84a9dc70d1a92555129d
GodMinseok <mseok830@naver.com>
2024-07-16 21:03:07 +0900

    Section 7. AOP (AOP가 필요한 상황)

--- Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/service/MemberService.java
+++ Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/service/MemberService.java
@@ -5,7 +5,6 @@ import hello.Inflearn_Spring_Beginner.repository.MemberRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 @Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
@@ -18,23 +17,32 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
+
     private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
     }
+
     /**
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 }
