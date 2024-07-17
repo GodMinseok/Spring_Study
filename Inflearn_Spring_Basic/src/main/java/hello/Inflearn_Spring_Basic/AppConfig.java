9038820a2f933877af6ab66ce448e8b0c4461d1f
GodMinseok <mseok830@naver.com>
2024-07-17 16:36:43 +0900

    Section 3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용 (새로운 구조와 할인 정책 적용)

--- Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/AppConfig.java
+++ Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/AppConfig.java
@@ -2,6 +2,7 @@ package hello.Inflearn_Spring_Basic;
 
 import hello.Inflearn_Spring_Basic.discount.DiscountPolicy;
 import hello.Inflearn_Spring_Basic.discount.FixDiscountPolicy;
+import hello.Inflearn_Spring_Basic.discount.RateDiscountPolicy;
 import hello.Inflearn_Spring_Basic.member.MemberRepository;
 import hello.Inflearn_Spring_Basic.member.MemberService;
 import hello.Inflearn_Spring_Basic.member.MemberServiceImpl;
@@ -23,7 +24,8 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
