dc892c3ddaefe256b4ea04c1ee1ee361fb4eefe7
GodMinseok <mseok830@naver.com>
2024-07-17 18:02:35 +0900

    Section 3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용 (스프링으로 전환하기)

--- Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/AppConfig.java
+++ Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/AppConfig.java
@@ -9,21 +9,24 @@ import hello.Inflearn_Spring_Basic.member.MemberServiceImpl;
 import hello.Inflearn_Spring_Basic.member.MemoryMemberRepository;
 import hello.Inflearn_Spring_Basic.order.OrderService;
 import hello.Inflearn_Spring_Basic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
-    private static MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/MemberApp.java
+++ Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/MemberApp.java
@@ -4,13 +4,20 @@ import hello.Inflearn_Spring_Basic.member.Grade;
 import hello.Inflearn_Spring_Basic.member.Member;
 import hello.Inflearn_Spring_Basic.member.MemberService;
 import hello.Inflearn_Spring_Basic.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
