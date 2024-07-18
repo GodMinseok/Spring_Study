155d49a700b027bd37b357faf4272e878e83244d
GodMinseok <mseok830@naver.com>
2024-07-18 18:50:22 +0900

    Section 4. 스프링 컨테이너와 스프링 빈 (다양한 설정 형식 지원 - 자바 코드, XML)

--- Inflearn_Spring_Basic/src/test/java/hello/Inflearn_Spring_Basic/beanfind/ApplicationContextExtendsFindTest.java
+++ Inflearn_Spring_Basic/src/test/java/hello/Inflearn_Spring_Basic/beanfind/ApplicationContextExtendsFindTest.java
@@ -26,7 +26,6 @@ public class ApplicationContextExtendsFindTest {
         DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
         assertThrows(NoUniqueBeanDefinitionException.class,
                 () -> ac.getBean(DiscountPolicy.class));
-
     }
 
     @Test
