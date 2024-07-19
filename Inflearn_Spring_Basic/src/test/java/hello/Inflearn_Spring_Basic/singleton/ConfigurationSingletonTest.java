8619f569aac33762ce3cd49ff73033369f418c16
GodMinseok <mseok830@naver.com>
2024-07-19 17:29:31 +0900

    Section 5. 싱글톤 컨테이너 (@Configuration과 싱글톤)

---
+++ Inflearn_Spring_Basic/src/test/java/hello/Inflearn_Spring_Basic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,34 @@
+package hello.Inflearn_Spring_Basic.singleton;
+
+import hello.Inflearn_Spring_Basic.AppConfig;
+import hello.Inflearn_Spring_Basic.member.MemberRepository;
+import hello.Inflearn_Spring_Basic.member.MemberServiceImpl;
+import hello.Inflearn_Spring_Basic.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberRepository1 -> memberRepository = " + memberRepository1);
+        System.out.println("memberRepository2 -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
