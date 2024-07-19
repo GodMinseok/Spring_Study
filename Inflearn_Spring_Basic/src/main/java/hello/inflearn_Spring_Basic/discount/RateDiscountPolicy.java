aa852d9c99e69876add4904a90b78dbc1f3b6b79
GodMinseok <mseok830@naver.com>
2024-07-20 01:18:34 +0900

    Section 7. 의존관계 자동 주입 (애노테이션 직접 만들기)

--- Inflearn_Spring_Basic/src/main/java/hello/inflearn_Spring_Basic/discount/RateDiscountPolicy.java
+++ Inflearn_Spring_Basic/src/main/java/hello/inflearn_Spring_Basic/discount/RateDiscountPolicy.java
@@ -1,10 +1,14 @@
 package hello.inflearn_Spring_Basic.discount;
 
+import hello.inflearn_Spring_Basic.annotation.MainDiscountPolicy;
 import hello.inflearn_Spring_Basic.member.Grade;
 import hello.inflearn_Spring_Basic.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
